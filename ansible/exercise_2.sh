2cf3de4dfad0464fc24188511dfea15cf559a619
PawelRakToJa <pawel.rak@ing.com>
2023-04-03 13:19:10 +0200

    added 2nd exercise

--- ansible/exercise_2.sh
+++ ansible/exercise_2.sh
@@ -6,6 +6,6 @@ while [ $filecount -lt 10000 ] ; do
     filesize=$RANDOM
     filesize=$(($filesize+1024))
     base64 /dev/urandom | 
-    head -c "$filesize" > /logfiles/file${filecount}."$(now)"
+    head -c "$filesize" > /logfiles/file${filecount}."$now"
     ((filecount++))
 done
