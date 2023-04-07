f8d0ff6678d6a4f1f205b4218337b0f8eadcb44b
PawelRakToJa <pawel.rak@ing.com>
2023-04-07 07:55:39 +0200

    changed 1st exercise

--- ansible/exercise_2.sh
+++ ansible/exercise_2.sh
@@ -2,10 +2,10 @@
 mkdir /logfiles
 filecount=0
 now=$( date +%Y%m%d-%H%M-%S )
-while [ $filecount -lt 10000 ] ; do
+while [ $filecount -lt 1000 ] ; do
     filesize=$RANDOM
     filesize=$(($filesize+1024))
     base64 /dev/urandom | 
-    head -c "$filesize" > /logfiles/file${filecount}."$now"
+    head -c "$filesize" > /var/log/file${filecount}."$now"
     ((filecount++))
 done
